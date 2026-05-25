fe61fa1d90c87b434cf5fb9ebb97a37ba28be7b9
Avinash <90386560+guycalledavinash@users.noreply.github.com>
2026-05-25 13:25:20 -0400

    Improve TaskTrackr scaffold with multi-module Spring Boot setup

--- java-backend-task-tracker/tasktrackr-user-service/src/main/java/com/tasktrackr/userservice/TaskTrackrUserserviceApplication.java
+++ java-backend-task-tracker/tasktrackr-user-service/src/main/java/com/tasktrackr/userservice/TaskTrackrUserserviceApplication.java
@@ -1,7 +1,12 @@
 package com.tasktrackr.userservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class TaskTrackrUserserviceApplication {
+
     public static void main(String[] args) {
-        System.out.println("user-service started...");
+        SpringApplication.run(TaskTrackrUserserviceApplication.class, args);
     }
 }
